4e32e4842efde86fafbf8bb04d0204dcf3f1bb87
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-27 21:57:43 -0600

    Update framework.js

--- framework.js
+++ framework.js
@@ -364,6 +364,34 @@
     return console.log(...arguments);
   };
 
+ globalThis.ifTry=(bool,then,elseThen)=>{
+    if(bool){
+        try{
+            if((typeof bool)=='function'){
+                if(bool()){
+                    return then();
+                }else{
+                    return elseThen();
+                }
+            }else{
+                return then();
+            }
+        }catch(e){
+            if(elseThen){
+                return elseThen(e);
+            }else{
+                return;
+            }
+        }
+    }else{
+        if(elseThen){
+            return elseThen(e);
+        }else{
+            return;
+        }
+    }
+}
+         
   globalThis.$ifTry = async (bool, then, elseThen) => {
     bool = await bool;
     if (bool) {
