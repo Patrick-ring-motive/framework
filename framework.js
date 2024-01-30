84fe8a76383c2ee516968b61e87e0ecec4ccb354
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-01-30 14:00:26 -0600

    Update framework.js

--- framework.js
+++ framework.js
@@ -1,6 +1,24 @@
  try {
+globalThis.jot=$=>{
+    let obj=Object.create(null);
+    obj.$=$;
+    Object.seal(obj);
+    return obj;
+    };
 
-	 
+globalThis.put=$=>{
+    let obj=Object.create(null);
+    obj.$=$;
+    Object.seal(obj);
+    return obj;
+    };
+    
+globalThis.fix=$=>{
+    const obj=Object.create(null);
+    obj.$=$;
+    Object.freeze(obj);
+    return obj;
+    };
   globalThis.await = (promise) => {
     console.log("Uncaught SyntaxError: await is only valid in async functions and the top level bodies of modules", promise);
     return promise;
@@ -1339,6 +1357,7 @@ return Object.keys(Object.getOwnPropertyDescriptors(obj))||[];
 }catch(e){return [];}
 }
 
+
 Object.forInKeys=function(obj){
 let keys = [];
 for(const i in obj){try{
@@ -1346,7 +1365,59 @@ keys.push[i];
 }catch(e){continue;}}
 return keys;
 }
-
+globalThis.assimilate=function(target,source){
+const objKeys=Object.descriptorKeys(source);
+const objKeys_length = objKeys.length;
+	 for(let i=0;i<objKeys_length;i++){try{
+		if((!target[objKeys[i]])&&(typeof source[objKeys[i]]=='function')){
+			target[objKeys[i]]=source[objKeys[i]];
+		}
+	 }catch(e){continue;}}
+return target;
+}
+globalThis.absorb=function(target,source){
+const objKeys=Object.descriptorKeys(source);
+const objKeys_length = objKeys.length;
+	 for(let i=0;i<objKeys_length;i++){try{
+		if((!target[objKeys[i]])&&(typeof source[objKeys[i]]=='function')){
+			target[objKeys[i]]=function(){
+			 return source[objKeys[i]](...arguments);
+			}
+		}
+	 }catch(e){continue;}}
+return target;
+}
+globalThis.mimic=function(target,source,proto){
+const objKeys=Object.descriptorKeys(proto);
+const objKeys_length = objKeys.length;
+	 for(let i=0;i<objKeys_length;i++){try{
+		if((!target[objKeys[i]])&&(typeof source[objKeys[i]]=='function')){
+			target[objKeys[i]]=function(){
+			 return source[objKeys[i]](...arguments);
+			}
+		}
+	 }catch(e){continue;}}
+return target;
+}
+assimilate(globalThis,Object);
+assimilate(globalThis,Reflect);
+assimilate(globalThis,Atomics);
+assimilate(globalThis,Math);
+assimilate(globalThis,JSON);
+assimilate(globalThis,BigInt);
+assimilate(globalThis,Date);
+absorb(globalThis,document);
+mimic(globalThis,document,Document.prototype);
+mimic(globalThis,document,EventTarget.prototype);
+globalThis.body =()=>document.body||document.firstElementChild;
+absorb(globalThis,body());
+mimic(globalThis,body(),HTMLBodyElement.prototype);
+mimic(globalThis,body(),HTMLElement.prototype);
+mimic(globalThis,body(),Element.prototype);
+mimic(globalThis,body(),Node.prototype);
+mimic(globalThis,body(),EventTarget.prototype);
+globalThis.select = globalThis.querySelector;
+globalThis.selects = globalThis.querySelectorAll;
   globalThis.swapText=function(startText,endText){
       let el=document.body;
       if(endText.toLowerCase().includes(startText.toLowerCase())){
