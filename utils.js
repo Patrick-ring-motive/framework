600c2368f3356a611c079bebcf8d537c1268447c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-01-04 21:45:07 -0600

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,40 @@
+
+export const instanceOf = function instanceOf(x,y){
+  try{
+    return x instanceof y;
+  }catch{
+    return false;
+  }
+};
+
+export const isNullish = function isNullish(x){
+  return x === undefined || x === null;
+};
+    
+export const isBoolean = function isBoolean(x){
+  return typeof x === 'boolean' || x instanceof Boolean;
+};
+
+export const isNumber = function isNumber(x){
+  return typeof x === 'number' || x instanceof Number;
+};
+
+export const isBigInt = function isBigInt(x){
+  return typeof x === 'bigint' || x instanceof BigInt;
+};
+
+export const isString = function isString(x){
+  return typeof x === 'string' || x instanceof String;
+};
+
+export const isSymbol = function isSymbol(x){
+  return typeof x === 'symbol' || x instanceof Symbol;
+};
+
+export const isObject = function isObject(x){
+  return typeof x === 'object' && x !== null;
+};
+
+export const isArray = function isArray(x){
+  return Array.isArray(x) || x instanceof Array;
+};
