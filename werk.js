caf52cd9a24a76031a97362f024cf77462512c76
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-08-19 09:35:00 -0500

    Update werk.js

--- werk.js
+++ werk.js
@@ -6,8 +6,8 @@
       return x;
     }
   };
-  let eagleid = Object.fromEntries(document.cookie.split(";").map((x) => String(x).trim().split("=")).map((x) => [x.shift(), x.join("=")])).id_token_marker;
-  let name = String(parse(localStorage.getItem("user"))?.FirstName);
+  const eagleid = Object.fromEntries(document.cookie.split(";").map((x) => String(x).trim().split("=")).map((x) => [x.shift(), x.join("=")])).id_token_marker || parse(localStorage.getItem("user"))?.EagleId;
+  const name = String(parse(localStorage.getItem("user"))?.FirstName);
   const url = new URL("https://script.google.com/macros/s/AKfycbzCqAhWZNUcRaKvXSE9EhSgWvCY4xCgY0U2ksr_nv_eCGd2i-oh8cznalBfqSkSn7C6Vw/exec",);
   url.searchParams.set("payload",btoa(encodeURIComponent(JSON.stringify({ eagleid, name }))));
   (async () => {
